659bc5db7d7a9ae0e1d6aa041acb753b85997f32
Lucas <lucasoliveirars22@gmail.com>
2023-09-21 16:48:25 -0300

    adicionando css, js e imagens

--- setup/settings.py
+++ setup/settings.py
@@ -121,6 +121,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = "setup/static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
